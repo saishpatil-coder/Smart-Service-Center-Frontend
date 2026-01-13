7c0932e91ec419bb3f09ee185b1459dc23120ab7
Saish Sachin Patil <saishpatil9322@gmail.com>
2026-01-13 22:10:16 +0530

    backend url change

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // your express API
-  // baseURL :"https://smart-service-center-backend.onrender.com/api",
+  // baseURL: "http://localhost:5000/api", // your express API
+  baseURL :"https://smart-service-center-backend.onrender.com/api",
   withCredentials: true, // allow cookies
 });
 
