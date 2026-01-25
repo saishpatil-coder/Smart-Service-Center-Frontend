a89a0cf6d692032c3ccb5b689f7cafdaeaf24aac
Saish Sachin Patil <saishpatil9322@gmail.com>
2026-01-25 22:21:40 +0530

    middleware fixes

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 import { NextResponse } from "next/server";
 
-=======
->>>>>>> 80b989d53d50e11268b3da8cad58d29f38bb8544
 
 export async function middleware(req) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/login"],
+};
